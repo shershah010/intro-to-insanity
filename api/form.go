2e4139c6e0fe0ba75467d7d588b5de72d890efe9
Sher Shah <sher.shah010@berkeley.edu>
2021-02-06 19:48:42 -0800

    removed

--- api/form.go
+++ api/form.go
@@ -3,13 +3,8 @@ package handler
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/graphql-go/graphql"
 )
-
-user_pass = make(map[string]string)
-user_pass["crow"] = "forest"
-user_pass["simple"] = "cipher"
 
 usernames = []string {
 	"crow",
@@ -28,9 +23,7 @@ hints = []string {
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-
 		fmt.Fprint(w, "SUCCESS")
-		// fmt.Fprint(w, hints[i])
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
