4517d29405e7a0e7d7b4a966059d04d5e9bdee6b
Sher Shah <sher.shah010@berkeley.edu>
2021-02-07 00:03:41 -0800

    fixes

--- src/scene.jsx
+++ src/scene.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Welcome } from './welcome';
 
 class Scene extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     switch (this.props.level) {
       case 0:
         return (<Welcome />);
       case 1:
         return (<Welcome />);
+      default:
+        return (<p>Uh..., this is a problem.</p>);
     }
   }
 } 
